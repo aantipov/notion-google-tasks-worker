339ff20c1888da863597b478055015d97e74d972
Alexey Antipov <antipov.alexei@gmail.com>
2023-11-27 16:57:26 +0100

    refactor schema

--- src/schema.ts
+++ src/schema.ts
@@ -2,19 +2,18 @@ import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 import type { GTokenResponseT } from '@/helpers/google-api';
 import type { NTokenResponseT } from '@/helpers/notion-api';
 
-type gTaskId = string;
-type nTaskId = string;
-type completedAt = string | null; // ISO date string '2023-10-25'
-
-type GTokenSafeT = Omit<GTokenResponseT, 'access_token'>;
+type GTaskIdT = string;
+type NTaskIdT = string;
+type CompletedAtT = string | null; // ISO date string '2023-10-25'
+type GTokenRestrictedT = Pick<GTokenResponseT, 'user' | 'refresh_token'>;
 
 export const users = sqliteTable('users', {
 	email: text('email').primaryKey(),
-	gToken: text('g_token', { mode: 'json' }).$type<GTokenSafeT>().notNull(),
+	gToken: text('g_token', { mode: 'json' }).$type<GTokenRestrictedT>().notNull(),
 	nToken: text('n_token', { mode: 'json' }).$type<NTokenResponseT>(),
 	tasklistId: text('tasklist_id'),
 	databaseId: text('database_id'),
-	mapping: text('mapping', { mode: 'json' }).$type<[gTaskId, nTaskId, completedAt?][]>(),
+	mapping: text('mapping', { mode: 'json' }).$type<[GTaskIdT, NTaskIdT, CompletedAtT?][]>(),
 	lastSynced: integer('last_synced', { mode: 'timestamp' }), // Important to recognize that sync was established successfully
 	created: integer('created', { mode: 'timestamp' }).notNull(),
 	modified: integer('modified', { mode: 'timestamp' }).notNull(),
@@ -23,11 +22,13 @@ export const users = sqliteTable('users', {
 // Use this table to derive a type of users that have been synced
 const __syncedUsers = sqliteTable('some-imaginary-table', {
 	email: text('email').primaryKey(),
-	gToken: text('g_token', { mode: 'json' }).$type<GTokenSafeT>().notNull(),
+	gToken: text('g_token', { mode: 'json' }).$type<GTokenRestrictedT>().notNull(),
 	nToken: text('n_token', { mode: 'json' }).$type<NTokenResponseT>().notNull(),
 	tasklistId: text('tasklist_id').notNull(),
 	databaseId: text('database_id').notNull(),
-	mapping: text('mapping', { mode: 'json' }).$type<[gTaskId, nTaskId, completedAt?][]>().notNull(),
+	mapping: text('mapping', { mode: 'json' })
+		.$type<[GTaskIdT, NTaskIdT, CompletedAtT?][]>()
+		.notNull(),
 	lastSynced: integer('last_synced', { mode: 'timestamp' }).notNull(), // Important to recognize that sync was established successfully
 	created: integer('created', { mode: 'timestamp' }).notNull(),
 	modified: integer('modified', { mode: 'timestamp' }).notNull(),
