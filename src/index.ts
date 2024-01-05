6c2c7a28f52eb5b4c738c9fd52b651ec7f7437fa
Alexey Antipov <antipov.alexei@gmail.com>
2024-01-05 19:27:43 +0100

    Setup sending emails using CRON

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,17 @@ const handler: ExportedHandler<Env, string> = {
 	 * It fetches users with  successfully established sync (lastSynced is not null)
 	 * who have been synced more than 5 minutes ago and
 	 */
-	async scheduled(_, env, ctx) {
-		ctx.waitUntil(scheduledHandler(env));
+	async scheduled(event, env, ctx) {
+		console.log('CRON job started', event.cron);
+		switch (event.cron) {
+			case '* * * * *':
+				ctx.waitUntil(handleScheduledSync(env));
+				break;
+			case '0 8 * * *': // Every day at 8:00 AM
+				ctx.waitUntil(handleScheduledSendAlmostUsers(env));
+				break;
+		}
+		console.log('CRON job finished');
 	},
 
 	/**
@@ -26,7 +35,43 @@ const handler: ExportedHandler<Env, string> = {
 
 export default handler;
 
-async function scheduledHandler(env: Env): Promise<void> {
+/**
+ * Send emails to users who haven't been synced for 24 hours
+ * and ask them if they need help
+ */
+async function handleScheduledSendAlmostUsers(env: Env): Promise<void> {
+	console.log('Sending emails to almost users');
+	// TODO: fetch emails from DB
+	const emails = ['alexey@notion-google-tasks-sync.com'];
+	// Use Mailjet API to send emails
+	// https://dev.mailjet.com/email/guides/send-api-v31/
+	const mailjetUrl = 'https://api.mailjet.com/v3.1/send';
+	const emailData = {
+		Globals: {
+			From: {
+				Email: 'alexey@notion-google-tasks-sync.com',
+				Name: 'Alexey Antipov',
+			},
+			Subject: `Just Checking In - How's Your Notion-Google Tasks Setup Going?`,
+			TextPart: getTextBody(),
+			HTMLPart: getHtmlBody(),
+		},
+		Messages: emails.map((Email) => ({
+			To: [{ Email }],
+		})),
+	};
+	const response = await fetch(mailjetUrl, {
+		method: 'POST',
+		headers: {
+			Authorization: 'Basic ' + btoa(`${env.MAILJET_API_KEY}:${env.MAILJET_SECRET_KEY}`),
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(emailData),
+	});
+	console.log('Mailjet response', response);
+}
+
+async function handleScheduledSync(env: Env): Promise<void> {
 	const db = drizzle(env.DB, { logger: true });
 	const fiveMinAgo = new Date(Date.now() - 5 * 60 * 1000);
 	let usersData: { email: string }[];
@@ -78,4 +123,86 @@ async function handleQueue(email: string, env: Env) {
 		console.error('Error handling queue', error);
 		throw new Error('Error handling queue', { cause: error });
 	}
+}
+
+function getHtmlBody() {
+	return `<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Notion-Google Tasks Sync Setup</title>
+		<style>
+			body {
+				font-family: Arial, sans-serif;
+				line-height: 1.6;
+			}
+			.email-container {
+				width: 80%;
+				margin: 0 auto;
+				padding: 20px;
+			}
+			.signature {
+				margin-top: 20px;
+			}
+			a {
+				color: #007bff;
+				text-decoration: none;
+			}
+			a:hover {
+				text-decoration: underline;
+			}
+			.ps {
+				margin-top: 10px;
+				font-style: italic;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="email-container">
+			<p>Hi there,</p>
+	
+			<p>I'm Alexey Antipov, the creator behind the Notion-Google Tasks Sync service. I noticed that you started setting up the sync but didn't get a chance to finish it. I wanted to personally check in and see if everything is okay.</p>
+	
+			<p>Is there something about the connection setup process that's holding you back? I understand these steps can sometimes be a bit tricky. If you're facing any challenges or have questions, I'm here to help. Just hit reply to this email, and I'll do my best to assist you.</p>
+	
+			<p>I created this service to make task management seamless and stress-free, and I'm really keen to see you get the most out of it. Your feedback and experience are invaluable to me.</p>
+	
+			<p>Looking forward to hearing from you!</p>
+	
+			<div class="signature">
+				Best,<br>
+				Alexey Antipov<br>
+				<a href="https://alexei.me/" target="_blank">alexei.me</a>
+			</div>
+	
+			<div class="ps">
+				P.S. If you’ve already finished setting up and have started using the service, I’d love to hear about your experience so far!
+			</div>
+		</div>
+	</body>
+	</html>
+	`;
+}
+
+function getTextBody() {
+	return `Hi there,
+
+
+	I'm Alexey Antipov, the creator behind the Notion-Google Tasks Sync service. I noticed that you started setting up the sync but didn't get a chance to finish it. I wanted to personally check in and see if everything is okay.
+	
+	
+	Is there something about the connection setup process that's holding you back? I understand these steps can sometimes be a bit tricky. If you're facing any challenges or have questions, I'm here to help. Just hit reply to this email, and I'll do my best to assist you.
+	
+	
+	I created this service to make task management seamless and stress-free, and I'm really keen to see you get the most out of it. Your feedback and experience are invaluable to me.
+	
+	
+	Looking forward to hearing from you!
+	
+	
+	Best,
+	Alexey Antipov
+	alexei.me [https://alexei.me/]
+	
+	
+	P.S. If you’ve already finished setting up and have started using the service, I’d love to hear about your experience so far!`;
 }
--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -1,6 +1,8 @@
 interface Env {
 	GOOGLE_CLIENT_ID: string;
 	GOOGLE_CLIENT_SECRET: string;
+	MAILJET_API_KEY: string;
+	MAILJET_SECRET_KEY: string;
 	QUEUE: Queue;
 	DB: D1Database;
 }
