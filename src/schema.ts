be46a10e482f52b42dacdaf1902517752818aa51
Alexey Antipov <antipov.alexei@gmail.com>
2024-02-15 17:18:30 +0100

    update sync_error.started type - make it required

--- src/schema.ts
+++ src/schema.ts
@@ -24,7 +24,7 @@ export const users = sqliteTable('users', {
 		num: number; // Number of consecutive sync errors
 		nextRetry: number | null; // Timestamp in ms. Null if no retries left. Max 10 retries within 5 days
 		sentEmail: boolean;
-		started?: number; // Timestamp in ms. undefined for old records
+		started: number; // Timestamp in ms. undefined for old records
 	}>(), // Last sync error message. Reset to null on successful sync
 	created: integer('created', { mode: 'timestamp' }).notNull(),
 	modified: integer('modified', { mode: 'timestamp' }).notNull(),
